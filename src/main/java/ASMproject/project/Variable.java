140c2040318abe4a11702166d3c7bb567b8d395e
XiaoLyu <blue_key2004@163.com>
2017-10-25 16:47:00 -0500

    variable declarations and variable references

--- src/main/java/ASMproject/project/Variable.java
+++ src/main/java/ASMproject/project/Variable.java
@@ -3,9 +3,9 @@ package ASMproject.project;
 
 import org.objectweb.asm.*;
 
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by lyuxiao on 10/24/17.
@@ -17,15 +17,17 @@ public class Variable extends MethodVisitor implements Opcodes {
 
     private int variableDeclarationNum = 0;
     private int variableReferenceNum = 0;
-//    private List<String> nameList = new ArrayList<>();
+    private List<String> nameList = new ArrayList<>();
 
     @Override
     public void visitLocalVariable(String name, String desc, String signature, Label start, Label end, int index) {
         variableDeclarationNum++;
 
+        // get rid of those primitive data types (the descriptors of them are single)
         if(!name.equals("this")){
             if(desc.length() > 1){
                 variableReferenceNum ++;
+                nameList.add(desc);
             }
         }
         super.visitLocalVariable(name, desc, signature, start, end, index);
@@ -42,7 +44,7 @@ public class Variable extends MethodVisitor implements Opcodes {
         return Math.max(variableReferenceNum, 0);
     }
 
-//    public List<String> getNameList(){
-//        return nameList;
-//    }
+    public List<String> getNameList(){
+        return nameList;
+    }
  }
