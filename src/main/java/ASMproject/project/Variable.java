a20b5ac0b0a55200bb736eddf65279fc15ad9c65
XiaoLyu <blue_key2004@163.com>
2017-10-25 16:11:32 -0500

    add variable references

---
+++ src/main/java/ASMproject/project/Variable.java
@@ -0,0 +1,48 @@
+package ASMproject.project;
+
+
+import org.objectweb.asm.*;
+
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+
+/**
+ * Created by lyuxiao on 10/24/17.
+ */
+public class Variable extends MethodVisitor implements Opcodes {
+    public Variable(MethodVisitor mv) {
+        super(ASM5, mv);
+    }
+
+    private int variableDeclarationNum = 0;
+    private int variableReferenceNum = 0;
+//    private List<String> nameList = new ArrayList<>();
+
+    @Override
+    public void visitLocalVariable(String name, String desc, String signature, Label start, Label end, int index) {
+        variableDeclarationNum++;
+
+        if(!name.equals("this")){
+            if(desc.length() > 1){
+                variableReferenceNum ++;
+            }
+        }
+        super.visitLocalVariable(name, desc, signature, start, end, index);
+    }
+
+    // get rid of this
+    // get the number of variables declared in the method
+    public int getVariableDeclarationNum() {
+        return Math.max(variableDeclarationNum - 1, 0);
+    }
+
+    // get the number of variables referenced in the method
+    public int getVariableReferenceNum(){
+        return Math.max(variableReferenceNum, 0);
+    }
+
+//    public List<String> getNameList(){
+//        return nameList;
+//    }
+ }
