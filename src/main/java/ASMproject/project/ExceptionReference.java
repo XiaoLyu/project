9d901acab0996dcbdaa99726e8bb4e13f4d67f82
XiaoLyu <blue_key2004@163.com>
2017-10-27 14:44:30 -0500

    add ExceptionReference.java

---
+++ src/main/java/ASMproject/project/ExceptionReference.java
@@ -0,0 +1,45 @@
+package ASMproject.project;
+
+import org.objectweb.asm.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyuxiao on 10/26/17.
+ * Exceptions referenced
+ */
+public class ExceptionReference extends ClassVisitor implements Opcodes {
+
+
+    public ExceptionReference(ClassVisitor cv) {
+        super(ASM5, cv);
+    }
+
+    private List<String> exceptionsNameList = new ArrayList<>();
+    private String exceptionsName = "";
+    private String temp = "";
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        for (int i = 0; i< exceptions.length; i++){
+            exceptionsNameList.add(exceptions[i]);
+            temp = exceptions[i];
+            String[] part = temp.split("/");
+            temp = part[part.length-1];
+            exceptionsName = exceptionsName + temp;
+            if( i < (exceptions.length - 1)){
+                exceptionsName = exceptionsName + " ";
+            }
+        }
+        return super.visitMethod(access, name, desc, signature, exceptions);
+    }
+
+    public List<String> getExceptionsNameList(){
+        return exceptionsNameList;
+    }
+
+    public String getExceptionsName(){
+        return exceptionsName;
+    }
+}
