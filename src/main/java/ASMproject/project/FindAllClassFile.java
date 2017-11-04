97163deb5832550b87acd3c0f73d42977002c41d
XiaoLyu <blue_key2004@163.com>
2017-11-04 16:32:46 -0500

    add FindAllClassFile.java

---
+++ src/main/java/ASMproject/project/FindAllClassFile.java
@@ -0,0 +1,32 @@
+package ASMproject.project;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyuxiao on 11/4/17.
+ * find all the .class files
+ */
+public class FindAllClassFile {
+
+    private List<String> p = new ArrayList<String>();
+
+    public List<String> findAllClassFile(String folder){
+        File directory = new File(folder);
+        File[] files = directory.listFiles();
+
+        for (File file : files) {
+            if (file.isFile()) {
+                if(file.getName().endsWith(".class")) {
+                    System.out.println("File: " + file.getAbsolutePath());
+                    p.add(file.getAbsolutePath());
+                }
+
+            } else {
+                findAllClassFile(file.toString());
+            }
+        }
+        return p;
+    }
+}
