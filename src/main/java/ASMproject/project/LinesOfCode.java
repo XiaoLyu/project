a1ea7d914ecbdae33d16f196eb06fd6730c45c39
XiaoLyu <blue_key2004@163.com>
2017-11-06 15:17:51 -0600

    remove the print message for 0

--- src/main/java/ASMproject/project/LinesOfCode.java
+++ src/main/java/ASMproject/project/LinesOfCode.java
@@ -7,21 +7,21 @@ import org.objectweb.asm.Opcodes;
 
 public class LinesOfCode extends MethodVisitor implements Opcodes {
 
-  private int lines = 0;
+    private int lines = 0;
 
-  public LinesOfCode(MethodVisitor mv) {
-    super(ASM5, mv);
-  }
+    public LinesOfCode(MethodVisitor mv) {
+        super(ASM5, mv);
+    }
 
-  @Override
-  public void visitLineNumber(int line, Label start) {
-    lines ++;
-    super.visitLineNumber(line, start);
-  }
+    @Override
+    public void visitLineNumber(int line, Label start) {
+        lines ++;
+        super.visitLineNumber(line, start);
+    }
 
     public int getLines() {
-        if (lines < 1)
-            System.out.printf("Lines computed to be %d, defaulting to 1\n", lines);
+//        if (lines < 1)
+//            System.out.printf("Lines computed to be %d, defaulting to 1\n", lines);
         return Math.max(lines, 1);
     }
 }
